ae6a22205928e253ed3c55a8c31322717e84ac46
Copperman5995 <cooper.d.mix@gmail.com>
2023-04-12 10:53:43 -0700

    made execute file

---
+++ execute.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void execute(char **argv)
+{
+	char *command = NULL;
+
+	if (argv)
+	{
+		command = argv[0];
+
+		if (execve(command, argv, NULL) == -1)
+		{
+			perror("Error:");
+		}
+	}
+}
